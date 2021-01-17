65f59fa7e81432f7dccfbc9a6cd5daae66dafd17
Home <homepage408@gmail.com>
2021-01-17 20:01:36 +0700

    tampil mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi Rest Api Berjalan", res);
 };
+
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (err, rows, field) {
+        if (err) {
+            console.log(err)
+        } else {
+            response.ok(rows, res)
+        }
+    });
+}
